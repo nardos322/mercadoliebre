a333c6c2502dc1b2005a938c650f2fcc2cfceb2b
nardos322 <nahuel.rlz@gmail.com>
2022-03-06 19:07:22 -0300

    se agrega formularios

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ app.get('/', (req, res) => {
     res.sendFile(home);
 })
 
+app.get('/registro', (req, res) => {
+    let register = path.join(__dirname, '/views/register.html');
+    res.sendFile(register);
+});
 
-
+app.get('/login', (req, res) => {
+    let login = path.join(__dirname, '/views/login.html');
+    res.sendFile(login);
+});
 
 
 
